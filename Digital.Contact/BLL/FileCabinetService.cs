fd056d296f0e2b491204bcb90ed109b0f7654ac8
HWNET_cp <SND\HWNET_cp>
2014-12-24 09:26:14 +0000

    add data services for company file cabinet model

---
+++ Digital.Contact/BLL/FileCabinetService.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Digital.Common;
+using Digital.Common.Utilities;
+using Digital.Common.Logging;
+using Digital.Contact.Models;
+using System.IO;
+
+namespace Digital.Contact.BLL
+{
+    public class FileCabinetService
+    {
+        protected static string DirectoryRoot { get; set; }
+
+        #region FileDirectoryName
+        public static string FileDirectoryName(string dirPath)
+        {
+            if (!Directory.Exists(dirPath))
+            {
+                return string.Empty;
+            }
+            return new DirectoryInfo(dirPath).Name;
+        }
+        #endregion
+
+        #region IsExistDirectoryRoot
+        protected bool IsExistDirectoryRoot(string UserId)
+        {
+            bool result=false;
+            var subFolder = "UserInfo" + CryptoService.MD5Encrypt(UserId,SerurityType.UserInfoFolder);
+            var rootDirectory = Path.Combine(UploadConfigContext.UploadPath, subFolder);
+
+            if (Directory.Exists(rootDirectory))
+            {
+                result = true;
+                FileCabinetService.DirectoryRoot = rootDirectory;
+            }
+            return result;
+        }
+        #endregion
+
+        #region FileDirectoryCreate
+        public bool FileDirectoryCreate(string UserId,string SubDirectoryName)
+        {
+            bool result = false;
+
+            if (this.IsExistDirectoryRoot(UserId))
+            {
+                var subDirectory = Path.Combine(FileCabinetService.DirectoryRoot, SubDirectoryName);
+                if (!Directory.Exists(subDirectory))
+                {
+                    var dirInfo=Directory.CreateDirectory(subDirectory);
+                    if (dirInfo != null && dirInfo.Exists)
+                    {
+                        result = true;
+                    }
+                }
+            }
+
+            return result;
+        }
+        #endregion
+
+        #region FileDirectoryPathByName
+        public string FileDirectoryPathByName(string UserId, string SubDirectoryName)
+        {
+            string result = string.Empty;
+
+            if (this.IsExistDirectoryRoot(UserId))
+            {
+                var subDirectory = Path.Combine(FileCabinetService.DirectoryRoot, SubDirectoryName);
+                if (Directory.Exists(subDirectory))
+                {
+                    result = subDirectory;
+                }
+            }
+
+            return result;
+        }
+        #endregion
+
+        #region FileDirectoryList
+        public List<FileFolderMode> FileDirectoryList(string UserId)
+        {
+            List<FileFolderMode> DirectoryList = new List<FileFolderMode>();
+            List<FilesMode> FileList = new List<FilesMode>();
+            if (this.IsExistDirectoryRoot(UserId))
+            {
+                DirectoryInfo DirectoryRoot = new DirectoryInfo(FileCabinetService.DirectoryRoot);
+                var lstDirectory = DirectoryRoot.GetDirectories();
+                foreach (var dir in lstDirectory)
+                {
+                    var FolderMode = new FileFolderMode
+                    { 
+                         FolderName=dir.Name,
+                         FolderPath=dir.FullName,
+                    };
+
+                    var FolderSize = this.FileDirectorySize(UserId, dir.FullName);
+                    FolderMode.FolderSize = FolderSize;
+                    FolderMode.FolderDate = dir.CreationTimeUtc;
+
+                    #region GetFiles
+                    var lstFiles = dir.GetFiles();
+                    foreach (var file in lstFiles)
+                    {
+                        var FilesMode = new FilesMode { 
+                            FolderName=dir.Name,
+                            FileName=file.Name,
+                            FileExtensionName=file.Extension,
+                            FilePath=file.FullName,
+                            FileSize=file.Length, 
+                            FileDate=file.CreationTimeUtc
+                        };
+
+                        FileList.Add(FilesMode);
+                    }
+
+                    if (FileList!=null&&FileList.Count>0)
+                    {
+                        FolderMode.FileList = FileList;
+                    }
+                    #endregion
+
+                    DirectoryList.Add(FolderMode);
+                }
+            }
+            return DirectoryList;
+        }
+        #endregion
+
+        #region FilesListByDirectory
+        
+        #endregion
+
+        #region FileDirectorySize
+        protected long FileDirectorySize(string UserId, string SubDirectoryName)
+        {
+            long dirSize = 0;
+            if (this.IsExistDirectoryRoot(UserId))
+            {
+                DirectoryInfo dir = new DirectoryInfo(SubDirectoryName);
+                foreach (FileInfo file in dir.GetFiles())
+                {
+                    dirSize += file.Length;
+                }
+
+                foreach (DirectoryInfo subdir in dir.GetDirectories())
+                {
+                    dirSize += this.FileDirectorySize(UserId, subdir.FullName);
+                }
+
+            }
+
+            return dirSize;
+        }
+        #endregion
+    }
+}
---
+++ Digital.Contact/Models/FileCabinetMode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital.Contact.Models
+{
+    public class FileFolderMode
+    {
+        public string FolderName { get; set; }
+        public string FolderPath { get; set; }
+        public long FolderSize { get; set; }
+        public DateTime FolderDate { get; set; }
+        public List<FilesMode> FileList { get; set; }
+    }
+    public class FilesMode
+    {
+        public string FolderName { get; set; }
+        public string FileName { get; set; }
+        public string FileExtensionName { get; set; }
+        public string FilePath { get; set; }
+        public long FileSize { get; set; }
+        public DateTime FileDate { get; set; }
+    }
+}
